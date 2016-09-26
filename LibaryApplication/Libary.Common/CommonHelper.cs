f170dacf1a596e62849cd6eab419295907b36d82
ProgramerKing <csharp_programer@163.com>
2016-09-26 21:32:08 +0800

    day1
    添加SqlHelper、CommonHelper
    
    day 1
    
    项目的创建！！
    
    :space_invader: Added .gitattributes & .gitignore files

---
+++ LibaryApplication/Libary.Common/CommonHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace Libary.Common
+{
+    using System.Security.Cryptography;
+    using System.Text;
+
+    public class CommonHelper
+    {
+        public string GetMd5(string txtStr)
+        {
+            //1.创建MD5对象
+            MD5 md5 = MD5.Create();
+            //2.将字符串转成字节数组
+            byte[] buffers = Encoding.UTF8.GetBytes(txtStr);
+            //3.加密
+            byte[] bytesMd5 = md5.ComputeHash(buffers);
+            //4.将字节数组转换成字符串
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bytesMd5.Length; i++)
+            {
+                sb.Append(bytesMd5[i].ToString("x2").ToLower());
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ LibaryApplication/Libary.DBUtility/ConnectionInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+
+namespace Libary.DBUtility
+{
+    /// <summary>
+    /// ConnectionInfo 
+    /// </summary>
+    public class ConnectionInfo
+    {
+        public static string GetSqlServerConnectionString()
+        {
+            return ConfigurationSettings.AppSettings["LibaryDB"];
+        }
+    }
+}
