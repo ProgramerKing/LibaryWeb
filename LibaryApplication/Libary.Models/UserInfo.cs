7aa3e753fd073655aadc65088d4e219f94babeba
ProgramerKing <csharp_programer@163.com>
2016-09-27 20:24:52 +0800

    1

---
+++ LibaryApplication/Libary.Models/UserInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libary.Models
+{
+    public class UserInfo
+    {
+        public int UserID { get; set; }
+        public string User_Name { get; set; }
+        public string User_Pwd { get; set; }
+        public string User_Email { get; set; }
+        public DateTime CreateTime { get; set; }
+    }
+}
